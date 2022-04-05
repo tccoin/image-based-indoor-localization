377203311546d91ab3a4ed55bc5d6f36d48ab563
Zhenyuan Zhang <cryscan@umich.edu>
2022-04-04 22:46:13 -0400

    Update search test.

--- src/search/main.cu
+++ src/search/main.cu
@@ -4,17 +4,30 @@
 
 #include <vector>
 #include <iostream>
+#include <chrono>
 
 #include "lib.h"
 
 int main() {
-    std::vector<double> data(128 * 4000);
-    std::vector<double> input(128, 1600);
+    std::vector<double> data(128 * 1000000);
 
     for (int i = 0; i < data.size(); ++i) data[i] = (double) i;
 
     init_data(data.data(), data.size());
-    std::cout << find_nearest(input.data());
+
+    auto total = std::chrono::high_resolution_clock::duration(0);
+
+    for (int i = 0; i < 100; ++i) {
+        std::vector<double> input(128, 1000 * i);
+
+        auto tic = std::chrono::high_resolution_clock::now();
+        find_nearest(input.data());
+        auto toc = std::chrono::high_resolution_clock::now();
+
+        total += toc - tic;
+    }
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(total).count();
 
     return 0;
 }
